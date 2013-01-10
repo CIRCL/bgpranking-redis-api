f2be1f527b047ce7a4de3d1a3231b3af53e11a4d
Raphaël Vinot <raphael@vinot.info>
2013-01-10 18:41:27 +0100

    json is a method in a requests.Response object, fixed.

--- example/api_web/client/api.py
+++ example/api_web/client/api.py
@@ -17,7 +17,7 @@ def all_ranks_single_asn(asn, last_day = None, timeframe = None,
             'timeframe': timeframe, 'dates_sources': dates_sources,
             'with_details_sources': with_details_sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def all_ranks_all_asns(last_day = None, timeframe = None,
         dates_sources = None, with_details_sources = None):
@@ -26,7 +26,7 @@ def all_ranks_all_asns(last_day = None, timeframe = None,
             'dates_sources': dates_sources,
             'with_details_sources': with_details_sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def asn_description(asn, date = None, sources = None):
     query = {'method': 'asn_description'}
@@ -34,7 +34,7 @@ def asn_description(asn, date = None, sources = None):
         sources = [sources]
     query.update({'asn': asn, 'date': date, 'sources': sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def ips_description(asn, asn_timestamp, date = None, sources = None):
     query = {'method': 'ips_description'}
@@ -43,25 +43,25 @@ def ips_description(asn, asn_timestamp, date = None, sources = None):
     query.update({'asn': asn, 'asn_timestamp': asn_timestamp,
             'date': date, 'sources': sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def stats(last_day = None, timeframe = None, dates_sources = None):
     query = {'method': 'stats'}
     query.update({'last_day': last_day,'timeframe': timeframe,
             'dates_sources': dates_sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def block_owner_description(asn, block):
     query = {'method': 'block_owner_description'}
     query.update({'asn': asn, 'block': block})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def cached_dates():
     query = {'method': 'cached_dates'}
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def cached_daily_rank(asn, date = None, sources = None):
     query = {'method': 'cached_daily_rank'}
@@ -69,7 +69,7 @@ def cached_daily_rank(asn, date = None, sources = None):
         sources = [sources]
     query.update({'asn': asn, 'date': date, 'sources': sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
 def cached_top_asns(date = None, source = None, limit = None,
         with_sources = None):
@@ -77,5 +77,5 @@ def cached_top_asns(date = None, source = None, limit = None,
     query.update({'date': date, 'source': source, 'limit': limit,
             'with_sources': with_sources})
     r = requests.post(url, data=json.dumps(query))
-    return r.json
+    return r.json()
 
