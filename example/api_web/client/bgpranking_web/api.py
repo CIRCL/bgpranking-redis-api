ace773d44f0f6f5e9a7bbb8d31bab0be0ec3d1e3
Raphaël Vinot <raphael@vinot.info>
2013-01-11 11:15:37 +0100

    Fix client REST API.

--- example/api_web/client/bgpranking_web/api.py
+++ example/api_web/client/bgpranking_web/api.py
@@ -10,14 +10,18 @@ import requests
 
 url = 'http://bgpranking.circl.lu/json'
 
+def __prepare_request(query):
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(query), headers=headers)
+    return r.json
+
 def all_ranks_single_asn(asn, last_day = None, timeframe = None,
         dates_sources = None, with_details_sources = None):
     query = {'method': 'all_ranks_single_asn'}
     query.update({'asn': asn, 'last_day': last_day,
             'timeframe': timeframe, 'dates_sources': dates_sources,
             'with_details_sources': with_details_sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def all_ranks_all_asns(last_day = None, timeframe = None,
         dates_sources = None, with_details_sources = None):
@@ -25,16 +29,14 @@ def all_ranks_all_asns(last_day = None, timeframe = None,
     query.update({'last_day': last_day, 'timeframe': timeframe,
             'dates_sources': dates_sources,
             'with_details_sources': with_details_sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def asn_description(asn, date = None, sources = None):
     query = {'method': 'asn_description'}
     if sources is not None and type(sources) is not type([]):
         sources = [sources]
     query.update({'asn': asn, 'date': date, 'sources': sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def ips_description(asn, asn_timestamp, date = None, sources = None):
     query = {'method': 'ips_description'}
@@ -42,40 +44,34 @@ def ips_description(asn, asn_timestamp, date = None, sources = None):
         sources = [sources]
     query.update({'asn': asn, 'asn_timestamp': asn_timestamp,
             'date': date, 'sources': sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def stats(last_day = None, timeframe = None, dates_sources = None):
     query = {'method': 'stats'}
     query.update({'last_day': last_day,'timeframe': timeframe,
             'dates_sources': dates_sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def block_owner_description(asn, block):
     query = {'method': 'block_owner_description'}
     query.update({'asn': asn, 'block': block})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def cached_dates():
     query = {'method': 'cached_dates'}
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def cached_daily_rank(asn, date = None, sources = None):
     query = {'method': 'cached_daily_rank'}
     if sources is not None and type(sources) is not type([]):
         sources = [sources]
     query.update({'asn': asn, 'date': date, 'sources': sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
 def cached_top_asns(date = None, source = None, limit = None,
         with_sources = None):
     query = {'method': 'cached_top_asns'}
     query.update({'date': date, 'source': source, 'limit': limit,
             'with_sources': with_sources})
-    r = requests.post(url, data=json.dumps(query))
-    return r.json()
+    return __prepare_request(query)
 
