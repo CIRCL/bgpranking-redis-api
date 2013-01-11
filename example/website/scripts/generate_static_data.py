4bd328d14f5997bb8cd605ace428f8f95d7f7ea5
Raphaël Vinot <raphael@vinot.info>
2013-01-11 13:16:02 +0100

    update public data in a loop

--- example/website/scripts/generate_static_data.py
+++ example/website/scripts/generate_static_data.py
@@ -2,18 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 
 from bgpranking import tools
 
 csv_dir = os.path.join('..', 'data', 'csv')
-tools.prepare_all_csv(csv_dir)
-
 agg_csv_dir = os.path.join('..', 'data', 'csv_agg')
-# cat LU-overall-allocv4-v6-asn.txt | grep asn | cut -d "|" -f 4
-lu_raw_asns = open('lu_asns_dump', 'r').read()
-lu_asns = lu_raw_asns.split('\n')
-
-tools.aggregate_csvs(csv_dir, agg_csv_dir, **{'luxembourg': lu_asns})
-
 js_dir = os.path.join('..', 'data', 'js')
-tools.generate_js_for_worldmap(js_dir)
+
+
+while True:
+    tools.prepare_all_csv(csv_dir)
+
+    # cat LU-overall-allocv4-v6-asn.txt | grep asn | cut -d "|" -f 4
+    lu_raw_asns = open('lu_asns_dump', 'r').read()
+    lu_asns = lu_raw_asns.split('\n')
+
+    tools.aggregate_csvs(csv_dir, agg_csv_dir, **{'luxembourg': lu_asns})
+
+    tools.generate_js_for_worldmap(js_dir)
+
+    time.sleep(10000)
