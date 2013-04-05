0d4f97dc95dbbcf0583445e3e1f6adab207ffe19
Raphaël Vinot <raphael@vinot.info>
2013-04-05 17:01:46 +0200

    make the csv exporter an infinite loop

--- example/export/asn_ranks/launch.sh
+++ example/export/asn_ranks/launch.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-bash ./launch_local_redis.sh
-python ./init_redis.py
+while true; do
+    bash ./launch_local_redis.sh
+    echo 'Preparing Redis database'
+    python ./init_redis.py
+    echo 'Done.'
 
-date
-for i in {1..10}; do
-    python ./consumer.py &
+    date
+    echo 'Exporting ranks to CSV....'
+    for i in {1..10}; do
+        python ./consumer.py &
+    done
+
+    python ./consumer.py
+    echo 'Export finished.'
+    date
+    sleep 10
+
+    redis-cli -s ./redis_export.sock shutdown
+
+    echo 'Building aggregations...'
+    python ./generate_aggs.py
+    echo 'Done.'
+    date
+
+    sleep 10000
 done
-
-python ./consumer.py
-date
-sleep 10
-
-redis-cli -s ./redis_export.sock shutdown
-
-bash ./generate_aggs.py
-
-date
