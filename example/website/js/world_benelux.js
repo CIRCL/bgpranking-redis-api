4828c95e69d26aa78cbf157ee2497a84c1743c95
Raphaël Vinot <raphael@vinot.info>
2013-05-29 09:51:03 +0200

    add benelux map

---
+++ example/website/js/world_benelux.js
@@ -0,0 +1,24 @@
+g = new Dygraph(
+    document.getElementById("graphdiv1"),
+    "csv_agg/belgium_netherlands_germany_luxembourg_france",
+    {
+      axes: {
+              y: {
+                  valueFormatter: function(y) {
+                      return y.toPrecision(8);
+                      },
+                  axisLabelFormatter: function(y) {
+                      return y.toPrecision(5);
+                      },
+                 }
+            },
+      legend: 'always',
+      fillGraph: true,
+      rollPeriod: 7,
+      showRoller: true,
+      logscale : true,
+      yAxisLabelWidth: 50,
+      xAxisLabelWidth: 60
+    }
+);
+
