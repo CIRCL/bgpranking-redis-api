6d52943966881b7365cc1dfc99b13125a9186cc6
Raphaël Vinot <raphael@vinot.info>
2013-06-09 20:41:23 +0200

    dump weight associated to the IP for the day.

--- example/export/day_ips/consumer.py
+++ example/export/day_ips/consumer.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
 
     r = redis.Redis(unix_socket_path='./redis_export.sock')
     date = r.get('date')
+    weights = bgpranking.get_all_weights(date)
     while True:
         asn_b = r.spop('asn_b')
         if asn_b is None:
@@ -18,7 +19,8 @@ if __name__ == '__main__':
         if len(ip_descs.get(block)) != 0:
             p = r.pipeline(False)
             for ip, sources in ip_descs.get(block).iteritems():
-                p.sadd('ips', ip)
+                p.zincrby('ips', ip, sum([int(weights[s])
+                    for s in sources]))
                 p.hmset(ip, {'asn': asn, 'block':block,
                     'sources': '|'.join(sources)})
             p.execute()
--- example/export/day_ips/dump.py
+++ example/export/day_ips/dump.py
@@ -7,18 +7,25 @@ import csv
 if __name__ == '__main__':
     r = redis.Redis(unix_socket_path='./redis_export.sock')
     with open('ips', 'w') as f:
-        ips = list(r.smembers('ips'))
+        ips = list(r.zrange('ips', 0, -1))
         for i in range(len(ips)):
             ips[i] = "%3s.%3s.%3s.%3s" % tuple(ips[i].split("."))
         ips.sort()
         for i in range(len(ips)):
             ips[i] = ips[i].replace(" ", "")
-        for ip in ips:
-            f.write(ip + '\n')
+        p = r.pipeline(False)
+        [p.zscore('ips', ip) for ip in ips]
+        weights = p.execute()
+        [f.write(','.join(iw) + '\n') for iw in zip(ips, weights)]
     with open('full', 'wb') as f:
         w = csv.writer(f)
-        ips = r.smembers('ips')
-        for ip in ips:
-            entries = r.hgetall(ip)
-            w.writerow([entries['asn'], entries['block'], ip, entries['sources']])
+        ips = r.zrange('ips', 0, -1)
+        p = r.pipeline(False)
+        [p.hgetall(ip) for ip in ips]
+        all_entries = p.execute()
+        i = 0
+        for entries in all_entries:
+            w.writerow([entries['asn'], entries['block'], ips[i],
+                entries['sources']])
+            i += 1
     r.shutdown()
