a3ffd20913c3c03011ef51b9db0dbbc88ac7cdac
Raphaël Vinot <raphael@vinot.info>
2013-08-16 15:56:23 +0200

    dump a list of days

--- example/export/day_ips/launch.sh
+++ example/export/day_ips/launch.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-bash ./launch_local_redis.sh
-python ./init_redis.py
+LIST_DATES="2012-01-01 2012-02-01 2012-03-01 2012-04-01 2012-05-01 2012-06-01
+2012-07-01 2012-08-01 2012-09-01 2012-10-01 2012-11-01 2012-12-01 2013-01-01
+2013-02-01 2013-03-01 2013-04-01 2013-05-01 2013-06-01 2013-07-01 2013-08-01"
 
-date
-for i in {1..10}; do
-    python ./consumer.py &
+for date in ${LIST_DATES}; do
+
+    bash ./launch_local_redis.sh
+    python ./init_redis.py -d $date
+
+    date
+    for i in {1..10}; do
+        python ./consumer.py &
+    done
+
+    python ./consumer.py
+    date
+    sleep 10
+
+    python ./dump.py
+    date
 done
-
-python ./consumer.py
-date
-sleep 10
-
-python ./dump.py
-date
