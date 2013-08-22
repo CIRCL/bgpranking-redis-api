f186a59b193269434833d83f03eeebbba88f7123
Raphaël Vinot <raphael@vinot.info>
2013-08-22 18:56:29 +0200

    use a file to list the dates of the IPs we want

--- example/export/day_ips/launch.sh
+++ example/export/day_ips/launch.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-LIST_DATES="2012-01-01 2012-02-01 2012-03-01 2012-04-01 2012-05-01 2012-06-01
-2012-07-01 2012-08-01 2012-09-01 2012-10-01 2012-11-01 2012-12-01 2013-01-01
-2013-02-01 2013-03-01 2013-04-01 2013-05-01 2013-06-01 2013-07-01 2013-08-01"
+while read date ; do
 
-for date in ${LIST_DATES}; do
-
+    echo $date
     bash ./launch_local_redis.sh
     python ./init_redis.py -d $date
 
@@ -20,4 +17,4 @@ for date in ${LIST_DATES}; do
 
     python ./dump.py
     date
-done
+done < dates.txt
