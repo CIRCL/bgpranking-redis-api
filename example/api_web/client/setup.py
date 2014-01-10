47f049928da81c2dcf94e058030b1e9c02b9d6b8
Raphaël Vinot <raphael@vinot.info>
2014-01-10 16:32:27 +0100

    make packages PyPi friendly

--- example/api_web/client/setup.py
+++ example/api_web/client/setup.py
@@ -3,21 +3,32 @@
 from distutils.core import setup
 
 try:
-       from distutils.command.build_py import build_py_2to3 as build_py
+    from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
-       from distutils.command.build_py import build_py
+    from distutils.command.build_py import build_py
 
 setup(
     name='bgpranking_web',
-    version='1.0.1',
+    version='1.0.2',
     description='Library to access the BGP Ranking REST API.',
-    url='https://github.com/Rafiot/bgpranking-py.git',
+    url='https://github.com/CIRCL/bgpranking-redis-api',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
-    maintainer_email='raphael.vinot@circl.lu',
     packages=['bgpranking_web'],
     cmdclass = {'build_py': build_py},
-    license='GNU GPLv3',
     long_description=open('README.md').read(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        ],
+
+    requires=['requests']
     )
--- setup.py
+++ setup.py
@@ -3,21 +3,31 @@
 from distutils.core import setup
 
 try:
-       from distutils.command.build_py import build_py_2to3 as build_py
+    from distutils.command.build_py import build_py_2to3 as build_py
 except ImportError:
-       from distutils.command.build_py import build_py
+    from distutils.command.build_py import build_py
 
 setup(
     name='bgpranking',
-    version='1.0.1',
-    description='API to access the Redis database of a BGP Ranking instance.',
-    url='https://github.com/Rafiot/bgpranking-py.git',
+    version='1.0.3',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
-    maintainer_email='raphael.vinot@circl.lu',
+    url='https://github.com/CIRCL/bgpranking-redis-api',
+    description='API to access the Redis database of a BGP Ranking instance.',
+    long_description=open('README.md').read(),
     packages=['bgpranking'],
     cmdclass = {'build_py': build_py},
-    license='GNU GPLv3',
-    long_description=open('README.md').read(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        ],
+    requires=['IPy', 'pubsublogger', 'redis', 'dateutil'],
     )
